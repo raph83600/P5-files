4acab774a65c10ee261c5d86019c44ae6da52ad9
raph montoy <raphmtya@yahoo.fr>
2022-09-28 10:50:53 +0200

    up

--- front/js/product.js
+++ front/js/product.js
@@ -1,6 +1,38 @@
-console.log ('je suis sur la page product')
+let str = document.location.href;
+let url = new URL(str);
+let search_params = new URLSearchParams(url.search); 
+let id = null;
+if(search_params.has('id')) {
+  id = search_params.get('id');
+  console.log(id)
+}
 
-console.log (window.location.search)
-let searchparams = new URLSearchParams(window.location.search)
-let id = searchparams.get('id')
-console.log (id)
+fetch('http://localhost:3000/api/products/' + id)
+  .then((response) => {
+   let responsejson = response.json()
+   return responsejson
+  })
+  .then((product) => //creation de la boucle pour appel des produits
+  { 
+
+  console.log(product)
+
+  //Images
+  let contenerimg = document.querySelector(".item__img");
+  contenerimg.innerHTML = `<img src="${product.imageUrl}" alt=${product.altTxt}>`;
+  
+  //nom
+  let productTitle = document.getElementById("title"); 
+  productTitle.innerHTML = product.name;
+
+  //prix
+  let productPrice = document.getElementById("price"); 
+  productPrice.innerHTML = product.price;
+
+  //description
+  let productDescription = document.getElementById("description"); 
+  productDescription.innerHTML = product.description;
+
+  const productColorsChoice = document.getElementById("colors");
+  console.log(colors);
+})
