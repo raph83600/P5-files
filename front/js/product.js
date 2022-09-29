82446cc9809080beec2cc4c8c5709a1a1873437b
raph montoy <raphmtya@yahoo.fr>
2022-09-29 12:13:32 +0200

    maj

--- front/js/product.js
+++ front/js/product.js
@@ -1,45 +1,45 @@
 let str = document.location.href;
 let url = new URL(str);
-let search_params = new URLSearchParams(url.search); 
+let search_params = new URLSearchParams(url.search);
 let id = null;
-if(search_params.has('id')) {
-  id = search_params.get('id');
-  console.log(id)
+if (search_params.has("id")) {
+  id = search_params.get("id");
+  console.log(id);
 }
 
-fetch('http://localhost:3000/api/products/' + id)
+fetch("http://localhost:3000/api/products/" + id)
   .then((response) => {
-   let responsejson = response.json()
-   return responsejson
+    let responsejson = response.json();
+    return responsejson;
   })
-  .then((product) => //creation de la boucle pour appel des produits
-  { 
+  .then(
+    (
+      product //creation de la boucle pour appel des produits
+    ) => {
+      console.log(product);
 
-  console.log(product)
+      //Images
+      let contenerimg = document.querySelector(".item__img");
+      contenerimg.innerHTML = `<img src="${product.imageUrl}" alt=${product.altTxt}>`;
 
-  //Images
-  let contenerimg = document.querySelector(".item__img");
-  contenerimg.innerHTML = `<img src="${product.imageUrl}" alt=${product.altTxt}>`;
-  
-  //nom
-  let productTitle = document.getElementById("title"); 
-  productTitle.innerHTML = product.name;
+      //nom
+      let productTitle = document.getElementById("title");
+      productTitle.innerHTML = product.name;
 
-  //prix
-  let productPrice = document.getElementById("price"); 
-  productPrice.innerHTML = product.price;
+      //prix
+      let productPrice = document.getElementById("price");
+      productPrice.innerHTML = product.price;
 
-  //description
-  let productDescription = document.getElementById("description"); 
-  productDescription.innerHTML = product.description;
+      //description
+      let productDescription = document.getElementById("description");
+      productDescription.innerHTML = product.description;
 
-  const productColorsChoice = document.getElementById("colors");
-  
-  for (let i = 0; i < productColorsChoice.length; i++) {
-   const ColorsChoice = document.createElement("option");
-   ColorsChoice.value = product.colors[i];
-   ColorsChoice.innerHTML = product.colors[i];
-   productColorsChoice.appendChild(ColorsChoice);
-  }
-
-})
+      const productColorsChoice = document.getElementById("colors");
+      for (let i = 0; i < productColorsChoice.length; i++) {
+        const ColorsChoice = document.createElement("option");
+        ColorsChoice.value = product.colors[i];
+        ColorsChoice.innerHTML = product.colors[i];
+        productColorsChoice.appendChild(ColorsChoice);
+      }
+    }
+  );
